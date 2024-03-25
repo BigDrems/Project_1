202a10182b96ed63a5ad574e7e6929ce6fdbd844
Jack <jackjonelfiel@gmail.com>
2024-03-26 06:26:13 +0800

    Change the css from inline to internal css

--- src/components/FoodListItems.jsx
+++ src/components/FoodListItems.jsx
@@ -3,17 +3,50 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const FoodListItems = ({item}) =>{
     return(
-        <View style={{backgroundColor:'gainsboro',padding:5,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-        <View style={{flex:1,gap:5}}>
-        <Text style={{color:'black', fontWeight:'bold',fontSize:16,padding:3,}}>
-            {item.label}
-        </Text>
-        <Text style={{fontSize:12,color:'gray',padding:3,letterSpacing:.8}}>{item.cal} cal, {item.quantity} pieces, {item.brand}</Text>
-        </View>
-        <AntDesign name="pluscircleo" size={24} color='royalblue' />
+        <View style = {style.container}>
+            <View style={style.insideContainer}>
+                <Text style={style.text}>
+                    {item.label}
+                </Text>
+                <Text style = {style.description}>
+                    {item.cal} cal, 
+                    {item.quantity} pieces, 
+                    {item.brand}
+                </Text>
+            </View>
+        <AntDesign name="pluscircleo" style= {style.button} />
   
         </View>
     );
   }
 
+  const style = StyleSheet.create({
+    container: {
+        backgroundColor:'gainsboro',
+        padding:5,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    insideContainer:{
+        flex:1,
+        gap:5,
+    },
+    text:{
+        color:'black', 
+        fontWeight:'bold',
+        fontSize:16,
+        padding:3,
+    },
+    description:{
+        fontSize:12,
+        color:'gray',
+        padding:3,
+        letterSpacing:.8,
+    },
+    button:{
+        fontSize: 24,
+        color:'royalblue',
+    },
+  });
   export default FoodListItems;
